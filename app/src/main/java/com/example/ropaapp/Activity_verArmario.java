ee3de66ecfa9dd2c39eb9941ba58626d0844f9b3
Chac092 <davitxu20@gmail.com>
2019-09-30 13:25:22 +0200

    Todo correctamente funcionando quiero cojer la foto

--- app/src/main/java/com/example/ropaapp/Activity_verArmario.java
+++ app/src/main/java/com/example/ropaapp/Activity_verArmario.java
@@ -1,18 +1,19 @@
 package com.example.ropaapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
-import android.os.Bundle;
-import android.provider.MediaStore;
-import android.widget.ImageView;
+        import android.content.Intent;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.graphics.drawable.BitmapDrawable;
+        import android.graphics.drawable.Drawable;
+        import android.net.Uri;
+        import android.os.Bundle;
+        import android.provider.MediaStore;
+        import android.widget.ImageView;
 
-import java.io.File;
-import java.io.IOException;
+        import java.io.File;
+        import java.io.IOException;
 
 public class Activity_verArmario extends AppCompatActivity {
     ImageView fotillo;
@@ -21,9 +22,9 @@ public class Activity_verArmario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_armario);
         fotillo = findViewById(R.id.Fotoprenda);
-        MediaStore.Images.Media.getContentUri("Foto0");
-        Bitmap bmp =  MediaStore.Images.Media.getBitmap(getContentResolver(),);
+        //Bitmap bmp =  MediaStore.Images.Media.getBitmap(getContentResolver(),);
+        Drawable drawable = new BitmapDrawable(getResources(), bmp);
         System.out.println("verarmario exitioso");
-        fotillo.setBackground();
+
     }
 }
