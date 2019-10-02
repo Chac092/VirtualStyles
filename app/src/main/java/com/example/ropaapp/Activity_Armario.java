08aec7e62d709f1840e82ce33818d3aba5f366c9
Chac092 <davitxu20@gmail.com>
2019-10-02 14:13:55 +0200

    Cojo foto pero quiero conseguir el id

--- app/src/main/java/com/example/ropaapp/Activity_Armario.java
+++ app/src/main/java/com/example/ropaapp/Activity_Armario.java
@@ -15,6 +15,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class Activity_Armario extends AppCompatActivity {
     Button Camisa;
     final static int cons =0;
@@ -45,6 +47,7 @@ public class Activity_Armario extends AppCompatActivity {
 
         subirFoto = findViewById(R.id.BTNSubirFoto);
         consultarFotos = findViewById(R.id.BTNPendientes);
+        checkwrittePermission();
         checkCameraPermission();
         subirFoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,6 +70,7 @@ public class Activity_Armario extends AppCompatActivity {
     public void sacarfoto(){
         Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(i,cons);
+
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -81,5 +85,16 @@ public class Activity_Armario extends AppCompatActivity {
     public void guardarFoto(){
         MediaStore.Images.Media.insertImage(getContentResolver(),bmp,"Foto"+contador, "");
         contador = contador+1;
+
+    }
+    private void checkwrittePermission(){
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Mensaje", "No se tiene permiso para la camara!.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 225);
+        } else {
+            Log.i("Mensaje", "Tienes permiso para usar la camara.");
+        }
     }
 }
--- app/src/main/java/com/example/ropaapp/Activity_verArmario.java
+++ app/src/main/java/com/example/ropaapp/Activity_verArmario.java
@@ -1,71 +1,79 @@
 package com.example.ropaapp;
 
         import androidx.appcompat.app.AppCompatActivity;
+        import androidx.core.app.ActivityCompat;
+        import androidx.core.content.ContextCompat;
+
         import android.Manifest;
+        import android.annotation.SuppressLint;
+        import android.app.Activity;
+        import android.content.Intent;
         import android.content.pm.PackageManager;
+        import android.graphics.Bitmap;
+        import android.graphics.BitmapFactory;
+        import android.graphics.drawable.Drawable;
         import android.net.Uri;
         import android.os.Build;
         import android.os.Bundle;
+        import android.os.Messenger;
+        import android.provider.MediaStore;
+        import android.util.Log;
         import android.widget.ImageView;
         import android.widget.Toast;
 
+        import java.io.FileNotFoundException;
+
+        import static java.security.AccessController.getContext;
+
 public class Activity_verArmario extends AppCompatActivity {
     ImageView fotillo;
-    Uri path;
-    private static final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+    Uri targetUri = Uri.parse("content://com.android.providers.media.documents/document/image%A43");
+    private static final int RQS_OPEN_IMAGE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_armario);
         fotillo = findViewById(R.id.Fotoprenda);
-
-        path = Uri.parse("content://com.android.providers.media.documents/document/image%A43");
-        //Drawable d = Drawable.createFromPath("content://com.android.providers.media.documents/document/image%A43");
-        //Bitmap bmp =  BitmapFactory.decodeFile();
-        //Drawable d = new BitmapDrawable(getResources(), bmp);
-        //fotillo.setImageURI(path);
-        checkPermission();
-        //ponerFoto();
+        checkDocumentPermission();
+        checkreadPermission();
+        Cojerdato();
+        //path = Uri.parse("content://com.android.providers.media.documents/document/image%A43");
     }
-    private void checkPermission() {
 
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-
-            Toast.makeText(this, "This version is not Android 6 or later " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
-
+    public void Cojerdato(){
+        Bitmap imagen = BitmapFactory.decodeFile("/storage/emulated/0/Pictures/1570012838183.jpg");
+        //Drawable imagen = getDrawable("/storage/emulated/0/Pictures/1570012838183.jpg");
+        fotillo.setImageBitmap(imagen);
+    }
+    private void checkDocumentPermission(){
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.MANAGE_DOCUMENTS);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Mensaje", "No se tiene permiso para la camara!.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.MANAGE_DOCUMENTS}, 225);
         } else {
-
-            int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
-
-            if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
-
-                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
-                        REQUEST_CODE_ASK_PERMISSIONS);
-
-                Toast.makeText(this, "Requesting permissions", Toast.LENGTH_LONG).show();
-
-            }else if (hasWriteContactsPermission == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this, "The permissions are already granted ", Toast.LENGTH_LONG).show();
-                ponerFoto();
-            }
-        }
-        return;
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if(REQUEST_CODE_ASK_PERMISSIONS == requestCode) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "OK Permissions granted ! " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
-                ponerFoto();
-            } else {
-                Toast.makeText(this, "Permissions are not granted ! " + Build.VERSION.SDK_INT, Toast.LENGTH_LONG).show();
-            }
-        }else{
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            Log.i("Mensaje", "Tienes permiso para usar la camara.");
         }
     }
-    public void ponerFoto(){
-        System.out.println("llego");
-        fotillo.setImageURI(path);
+    private void checkreadPermission(){
+        int permissionCheck = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Log.i("Mensaje", "No se tiene permiso para la camara!.");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 225);
+        } else {
+            Log.i("Mensaje", "Tienes permiso para usar la camara.");
+        }
     }
-}
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Cojerdato();
+        }
+    }
+    }
+
+
+
+
