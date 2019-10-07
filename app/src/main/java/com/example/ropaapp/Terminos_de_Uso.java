9b89b855af4c520e8559f8475c5955dfa8f3db4e
EduardoCG12 <50154205+EduardoCG12@users.noreply.github.com>
2019-10-07 13:19:05 +0200

    terminos

---
+++ app/src/main/java/com/example/ropaapp/Terminos_de_Uso.java
@@ -0,0 +1,13 @@
+package com.example.ropaapp;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Terminos_de_Uso extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
