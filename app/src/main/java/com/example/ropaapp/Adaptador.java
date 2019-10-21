d94c1013a33b4d0f343f7761611e81292929d759
EduardoCG12 <eduardo.cosplayer@gmail.com>
2019-10-21 12:43:50 +0200

    Merge branch 'master' of https://github.com/Chac092/VirtualStyles into Eduardo
    # Conflicts:
    #	app/src/main/res/layout/activity__terminos.xml
    #	app/src/main/res/layout/activity_menuy_selecciondeclientes.xml
    #	app/src/main/res/layout/activity_rellenar_prendas.xml
    #	app/src/main/res/layout/activity_seleccion_prenda.xml

--- app/src/main/java/com/example/ropaapp/Adaptador.java
+++ app/src/main/java/com/example/ropaapp/Adaptador.java
@@ -1,27 +1,18 @@
 package com.example.ropaapp;
 
 import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
-import static com.example.ropaapp.R.*;
+import static com.example.ropaapp.R.drawable;
+import static com.example.ropaapp.R.layout;
 
 public class Adaptador extends RecyclerView.Adapter <Adaptador.ViewHolder> {
     private View.OnClickListener onClickListener;
